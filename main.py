a0297e2d6afbdb3f19e839e9fe8dd31616afc217
Kevin Bowrin <kevin.bowrin@carleton.ca>
2018-12-11 16:33:00 -0500

    First working version

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import random
 import string
 import config
 import sys
+from operator import itemgetter
+from tabulate import tabulate
 
 @click.group()
 def cli():
@@ -19,9 +21,10 @@ def cli():
 @cli.command()
 @click.option('--records', default=0, help="Number of records in the input, use to skip counting records.")
 @click.option('--delete/--no-delete', default=True, help=f"Delete the {config.table} table before loading.")
-@click.option('--bibid', default='001', help=f"Where is the unique bibid stored.")
+@click.option('--bibidselector', default='001', help=f"Where is the unique bibid stored. ex: 001 or 907a")
+@click.option('--trimbibid/--no-trimbibid', default=True, help=f"Delete the last character in the bibid.")
 @click.argument('input', type=click.Path(exists=True, dir_okay=False, resolve_path=True, readable=True))
-def load(records, delete, bibid, input):
+def load(records, delete, bibidselector, trimbibid, input):
     """Load a marc file into the database."""
     click.echo("Loading MARC data into database.")
 
@@ -54,21 +57,18 @@ def load(records, delete, bibid, input):
     q = multiprocessing.Queue()
     processes = []
     for i in range(multiprocessing.cpu_count()):
-        p = multiprocessing.Process(target=loader, args=(q, i, bibid))
+        p = multiprocessing.Process(target=loader, args=(q, bibidselector, trimbibid))
         processes.append(p)
         p.start()
 
     numprocessed = 0
     with open(input, 'rb') as f:
-        reader = pymarc.MARCReader(f, to_unicode=True)
+        reader = pymarc.MARCReader(f, to_unicode=True, force_utf8=True)
         with click.progressbar(reader,
                        label='Processing MARC records',
                        length=numrecords) as reader:
             for record in reader:
-                bibidfield = record[str(bibid)]
-                if bibid == None:
-                    click.echo(record.as_dict())
-                    sys.exit(1)
+                getBibID(record, bibidselector, trimbibid)
                 q.put(record)
                 while q.qsize() > 100:
                     time.sleep(1)
@@ -80,18 +80,17 @@ def load(records, delete, bibid, input):
 
     click.echo("Done, processed {} records.".format(numprocessed))
 
-def loader(inputqueue, id, bibid):
+def loader(inputqueue, bibidselector, trimbibid):
     num = 0
     conn = psycopg2.connect(f"dbname={config.database} user={config.username} password={config.password}")
     cur = conn.cursor()
     for record in iter(inputqueue.get, None):
-        bibidvalue = record[bibid].value()
+        bibid = getBibID(record, bibidselector, trimbibid)
         for field in record.get_fields():
-            if field.tag != '001':
-                subfields = getattr(field, 'subfields', [" ", field.value()])
-                for subfield, value in zip(subfields[0::2], subfields[1::2]):
-                    cur.execute(sql.SQL("INSERT INTO {} (bibid, tag, indicator1, indicator2, subfield, value) VALUES (%s, %s, %s, %s, %s, %s)").format(sql.Identifier(config.table)),
-                                 (bibidvalue, field.tag, getattr(field, 'indicator1', ""), getattr(field, 'indicator2', ""), subfield, value))
+            subfields = getattr(field, 'subfields', [" ", field.value()])
+            for subfield, value in zip(subfields[0::2], subfields[1::2]):
+                cur.execute(sql.SQL("INSERT INTO {} (bibid, tag, indicator1, indicator2, subfield, value) VALUES (%s, %s, %s, %s, %s, %s)").format(sql.Identifier(config.table)),
+                             (bibid, field.tag, getattr(field, 'indicator1', ""), getattr(field, 'indicator2', ""), subfield, value))
         num+=1
         if num % 1000 == 0:
             conn.commit()
@@ -137,10 +136,118 @@ def gremlin(delete, change, add):
     cur.close()
     conn.close()
 
+
 @cli.command()
-def check():
+@click.option('--records', default=0, help="Number of records in the input, use to skip counting records.")
+@click.option('--bibidselector', default='001', help=f"Where is the unique bibid stored. ex: 001 or 907a")
+@click.option('--trimbibid/--no-trimbibid', default=True, help=f"Delete the last character in the bibid.")
+@click.argument('input', type=click.Path(exists=True, dir_okay=False, resolve_path=True, readable=True))
+def check(records, bibidselector, trimbibid, input):
     """Check a marc file against a database to ensure no unexpected changes occurred."""
-    click.echo('check')
+    click.echo("Checking MARC data against database.")
+
+    numrecords = 0
+    if records == 0:
+        click.echo("Counting records in input file...", nl=False)
+        with open(input, 'rb') as f:
+            mm = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
+            while True:
+                cur = mm.read(1)
+                if cur:
+                    if cur == b'\x1d':
+                        numrecords+=1
+                else:
+                    break
+        click.echo(" done.")
+    else:
+        numrecords = records
+
+    q = multiprocessing.Queue()
+    printq = multiprocessing.Queue()
+    processes = []
+    for i in range(multiprocessing.cpu_count()-1):
+        p = multiprocessing.Process(target=compare, args=(q, bibidselector, printq, trimbibid))
+        processes.append(p)
+        p.start()
+
+    printer = multiprocessing.Process(target=printfromqueue, args=(printq,))
+    printer.start()
+
+    numprocessed = 0
+    with open(input, 'rb') as f:
+        reader = pymarc.MARCReader(f, to_unicode=True, force_utf8=True)
+        with click.progressbar(reader,
+                       label='Processing MARC records',
+                       length=numrecords) as reader:
+            for record in reader:
+                getBibID(record, bibidselector, trimbibid)
+                q.put(record)
+                while q.qsize() > 100:
+                    time.sleep(1)
+                numprocessed+=1
+        for process in processes:
+            q.put(None)
+        for process in processes:
+            process.join()
+
+    printq.put(None)
+    printer.join()
+
+    click.echo("Done, processed {} records.".format(numprocessed))
+
+def compare(inputqueue, bibidselector, printq, trimbibid):
+    num = 0
+    conn = psycopg2.connect(f"dbname={config.database} user={config.username} password={config.password}")
+    for record in iter(inputqueue.get, None):
+        bibid = getBibID(record, bibidselector, trimbibid)
+        setofrowsdb = set()
+        setofrowsfile = set()
+        readcur = conn.cursor()
+        readcur.execute(sql.SQL("SELECT bibid, tag, indicator1, indicator2, subfield, value FROM {} WHERE bibid = %s").format(sql.Identifier(config.table)), (bibid,))
+        for row in readcur:
+            setofrowsdb.add(row)
+
+        for field in record.get_fields():
+            subfields = getattr(field, 'subfields', [" ", field.value()])
+            for subfield, value in zip(subfields[0::2], subfields[1::2]):
+                setofrowsfile.add((bibid, field.tag, getattr(field, 'indicator1', ""), getattr(field, 'indicator2', ""), subfield, value))
+
+        printq.put((bibid, setofrowsdb, setofrowsfile))
+
+    conn.close()
+
+def printfromqueue(queue):
+    for bibidvalue, setofrowsdb, setofrowsfile in iter(queue.get, None):
+        click.echo(f"----Report for {bibidvalue}----\n")
+
+        dbminusfile = setofrowsdb.difference(setofrowsfile)
+        if len(dbminusfile) != 0:
+            sorteddbminusfile = sorted(list(dbminusfile), key=itemgetter(1,2,3,4,5))
+            click.echo("In database not in file:")
+            click.echo(tabulate(sorteddbminusfile, headers=["bibid", "tag", "indicator1", "indicator2", "subfield", "value"]))
+            click.echo("")
+
+        fileminusdb = setofrowsfile.difference(setofrowsdb)
+        if len(fileminusdb) != 0:
+            sortedfileminusdb = sorted(list(fileminusdb), key=itemgetter(1,2,3,4,5))
+            click.echo("In file not in database:")
+            click.echo(tabulate(sortedfileminusdb, headers=["bibid", "tag", "indicator1", "indicator2", "subfield", "value"]))
+            click.echo("")
+
+def getBibID(record, bibidselector, trimbibid):
+    bibid = None
+    if len(bibidselector) == 4:
+        bibid = record[bibidselector[0:3]][bibidselector[3]]
+    else:
+        bibid = record[bibidselector]
+
+    if bibid == None:
+        click.echo(record.as_dict())
+        sys.exit(1)
+    else:
+        if trimbibid:
+            bibid = bibid[:-1]
+        return bibid
 
 if __name__ == '__main__':
     cli()
